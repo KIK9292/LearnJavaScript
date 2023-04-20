167e8fc9731c428b2be64391c468378f3d42f089
KirillKozlo <116728298+KIK9292@users.noreply.github.com>
2023-04-20 21:16:12 +0300

    new freecodecamp

--- TEST/test.js
+++ TEST/test.js
@@ -1,38 +1,8 @@
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
+// Setup
+const myArr = [2, 3, 4, 5, 6];
 
 // Only change code below this line
-function updateRecords(records, id, prop, value) {
-  debugger
-  if((prop==="traks")&&(value!=="")){
-    debugger
-    if(records.id.hasOwnProperty("traks")){
-      debugger
-      records.id.tracks.push(prop);
-      debugger
-    }else{records.id["tracks"]=[];
-      debugger
-    records.id.tracks.push(prop);
-debugger
-    }
-  }
-  return records;
+let total =0;
+for(let i = 0; i< myArr.length;i++){
+  total += myArray[i];
 }
-console.log(updateRecords(recordCollection, 2468, "tracks", "Free"));
